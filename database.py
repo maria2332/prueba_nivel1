2ec72f3feebac631aed93fb6f0656b7b1b0e0718
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-02 14:00:24 +0100

    cambio

--- database.py
+++ database.py
@@ -12,70 +12,73 @@ class vehiculo():
 
 class Coche(vehiculo):
     def __init__(self, color, ruedas, velocidad, cilindrada):
-        vehiculo.__init__(self, color, ruedas)
+        super().__init__(color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
-            return vehiculo.__str__(self) + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
+            return super().__str__() + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
     
 class Bicicleta(vehiculo):
     def __init__(self, color, ruedas, tipo):
-        vehiculo.__init__(self, color, ruedas)
+        super().__init__(color, ruedas)
         self.tipo = tipo
     def __str__(self):
-             return vehiculo.__str__(self) + ", Tipo: " + str(self.tipo)  
+             return super().__str__() + ", Tipo: " + str(self.tipo)  
 
 
 class camioneta(Coche):
      def __init__(self, color, ruedas, velocidad, cilindrada, carga):
-          Coche.__init__(self, color, ruedas, velocidad, cilindrada)
+          super().__init__(color, ruedas, velocidad, cilindrada)
           self.carga = carga
      def __str__(self):
-            return Coche.__str__(self) + ", Carga: " + str(self.carga)
+            return super().__str__() + ", Carga: " + str(self.carga)
 
 class Motocicleta(Bicicleta):
     def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
-        vehiculo.__init__(self, color, ruedas, tipo)
+        super().__init__(color, ruedas, tipo)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
-            return vehiculo.__str__(self) + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
+            return super().__str__() + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
      
  
 class Quad(vehiculo):
     def __init__(self, color, ruedas, velocidad, cilindrada):
-        Coche.__init__(self, color, ruedas) and Bicicleta.__init__(self, color, ruedas)
+        super.__init__(color, ruedas) and Bicicleta.__init__(self, color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
-            return vehiculo.__str__(self) + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
+            return super().__str__() + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
     
 
-def catalogar(lista_vehiculos):
-    for vehiculo in lista_vehiculos:
-        print (vehiculo)
-        ruedas = vehiculo.ruedas
-        if ruedas == 2:
-            print("Es una bicicleta o una motocicleta")
-        elif ruedas == 4:
-            print("Es un coche o una camioneta")
-        else:
-            print("No es un vehículo")
-    return lista_vehiculos   
+# def catalogar(lista_vehiculos):
+#     for vehiculo in lista_vehiculos:
+#         print (vehiculo)
+#         ruedas = vehiculo.ruedas
+#         if ruedas == 2:
+#             print("Es una bicicleta o una motocicleta")
+#         elif ruedas == 4:
+#             print("Es un coche o una camioneta")
+#         else:
+#             print("No es un vehículo")
+#     return lista_vehiculos   
 
-# c = Coche("azul", 4, 150, 1200)
-# print(c)
+c = Coche("azul", 4, 150, 1200)
+print(c)
 
-# b = Bicicleta("roja", 2, "urbana")
-# print(b)
+b = Bicicleta("roja", 2, "urbana")
+print(b)
 
-# m = Motocicleta("negra", 2, "urbana", 200, 600)
-# print(m)
+m = Motocicleta("negra", 2, "urbana", 200, 600)
+print(m)
 
-# cam = camioneta("blanca", 4, 100, 2000, 1000)
-# print(cam)
+cam = camioneta("blanca", 4, 100, 2000, 1000)
+print(cam)
 
-lista_vehiculos = [Coche("azul", 4, 150, 1200), Bicicleta("roja", 2, "urbana"), Motocicleta("negra", 2, "urbana", 200, 600), camioneta("blanca", 4, 100, 2000, 1000)]
+q = Quad("verde", 4, 150, 1200)
+print(q)
+
+# lista_vehiculos = [Coche("azul", 4, 150, 1200), Bicicleta("roja", 2, "urbana"), Motocicleta("negra", 2, "urbana", 200, 600), camioneta("blanca", 4, 100, 2000, 1000)]
 
 
-catalogar(lista_vehiculos)  
+# catalogar(lista_vehiculos)  
