696e5f165d096c5dce14b1254dd413d87d53f9d1
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-02 09:36:10 +0100

    cambio

--- ejercicio.py
+++ ejercicio.py
@@ -2,7 +2,6 @@ class vehiculo():
     def __init__(self, color, ruedas):
         self.color = color 
         self.ruedas = ruedas
-
     def __str__(self):
         return "Color: " + self.color + ", Ruedas: " + str(self.ruedas)
     
@@ -13,7 +12,6 @@ class Coche(vehiculo):
         vehiculo.__init__(self, color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
-
     def __str__(self):
             return vehiculo.__str__(self) + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
     
@@ -21,7 +19,6 @@ class Bicicleta(vehiculo):
     def __init__(self, color, ruedas, tipo):
         vehiculo.__init__(self, color, ruedas)
         self.tipo = tipo
-
     def __str__(self):
              return vehiculo.__str__(self) + ", Tipo: " + str(self.tipo)  
 
@@ -31,7 +28,6 @@ class camioneta(Coche):
      def __init__(self, color, ruedas, velocidad, cilindrada, carga):
           Coche.__init__(self, color, ruedas, velocidad, cilindrada)
           self.carga = carga
-
      def __str__(self):
             return Coche.__str__(self) + ", Carga: " + str(self.carga)
 
@@ -40,32 +36,31 @@ class Motocicleta(Bicicleta):
         vehiculo.__init__(self, color, ruedas, tipo)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
-
     def __str__(self):
             return vehiculo.__str__(self) + ", Velocidad: " + str(self.velocidad) + ", Cilindrada: " + str(self.cilindrada)
      
  
     
-# lista_vehiculos = [Coche("azul", 4, 150, 1200), Bicicleta("roja", 2, "urbana"), Motocicleta("negra", 2, "urbana", 200, 600), camioneta("blanca", 4, 100, 2000, 1000)]
+lista_vehiculos = [Coche("azul", 4, 150, 1200), Bicicleta("roja", 2, "urbana"), Motocicleta("negra", 2, "urbana", 200, 600), camioneta("blanca", 4, 100, 2000, 1000)]
 
 
 
-c = Coche("azul", 4, 150, 1200)
-print(c)
+# c = Coche("azul", 4, 150, 1200)
+# print(c)
 
-b = Bicicleta("roja", 2, "urbana")
-print(b)
+# b = Bicicleta("roja", 2, "urbana")
+# print(b)
 
-m = Motocicleta("negra", 2, "urbana", 200, 600)
-print(m)
+# m = Motocicleta("negra", 2, "urbana", 200, 600)
+# print(m)
 
-cam = camioneta("blanca", 4, 100, 2000, 1000)
-print(cam)
+# cam = camioneta("blanca", 4, 100, 2000, 1000)
+# print(cam)
 
 
-# def catalogar(lista_vehiculos):
-#     for vehiculo in lista_vehiculos:
-#         print (vehiculo)
-#     return vehiculo
+def catalogar(lista_vehiculos):
+    for vehiculo in lista_vehiculos:
+        print (vehiculo)
+    return lista_vehiculos
 
-# catalogar(lista_vehiculos)  
+catalogar(lista_vehiculos)  
